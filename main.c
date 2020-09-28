4f02933adc90e75f4d530a299ca53c8b0bccd886
MrFMach <mrfmach@gmail.com>
2020-09-28 19:43:33 -0300

    reverse byte by byte

--- main.c
+++ main.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void incremet(char arg[])
 {
     puts(arg);
     __int8_t a = 5, *ptr_a = &a;
     printf("%X + 1 sizeof(int8)   is  1 byte  -> %X \n", ptr_a, ptr_a + 1);
-    system("sleep 0.5");
+    system("sleep 0.3");
     __int16_t b = 5, *ptr_b = &b;
     printf("%X + 1 sizeof(int16)  is  2 bytes -> %X \n", ptr_b, ptr_b + 1);
-    system("sleep 0.5");
+    system("sleep 0.3");
     __int32_t c = 5, *ptr_c = &c;
     printf("%X + 1 sizeof(int32)  is  4 bytes -> %X \n", ptr_c, ptr_c + 1);
-    system("sleep 0.5");
+    system("sleep 0.3");
     __int64_t d = 5, *ptr_d = &d;
     printf("%X + 1 sizeof(int64)  is  8 bytes -> %X \n", ptr_d, ptr_d + 1);
-    system("sleep 0.5");
+    system("sleep 0.3");
     __int128_t e = 5, *ptr_e = &e;
     printf("%X + 1 sizeof(int128) is 16 bytes -> %X \n", ptr_e, ptr_e + 1);
-    system("sleep 0.5");
+    system("sleep 0.3");
 }
 
 void decrement(char arg[])
@@ -39,16 +38,18 @@ void decrement(char arg[])
     }
 
     puts("\n<< String in reverse >>");
-    
+
     while (ptr >= &str)
     {
-        putchar(*ptr--);  
+        printf("%c\n", *ptr);
+        system("sleep 0.3");
+        *ptr--;
     }
 }
 
 void main(void)
 {
-    incremet("\n<< Poiter Increment Example with Address in Hexadecimal >>");
-    decrement("\n<< Poiter Decrement Example with String >>");
+    incremet("\n<< Pointer Increment Example with Address in Hexadecimal >>");
+    decrement("\n<< Pointer Decrement Example with String >>");
     puts("");
 }
